986e26dfd895d0bd741a0c94744b6fd37b0c79dc
Dat Le <73367400+datle2001@users.noreply.github.com>
2023-11-19 16:49:32 -0800

    change

--- src/app/top/top.component.ts
+++ src/app/top/top.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CartServices } from '../services/cart.service';
+import { S3Services } from '../services/s3.service';
 
 @Component({
   selector: 'top-root',
   templateUrl: './top.component.html',
   styleUrls: ['./top.component.css'],
 })
-export class TopComponent {
-  constructor(protected cartService: CartServices) {}
+export class TopComponent implements OnInit {
+  constructor(protected cartService: CartServices, private s3: S3Services) {}
+
+  imageLinks: { [imageName: string]: string } = {
+    shop_logo: '',
+    account_logo: '',
+  };
+
+  ngOnInit(): void {
+    this.getImageLinks();
+  }
+
+  getImageLinks() {
+    Object.keys(this.imageLinks).forEach((imageName) => {
+      this.imageLinks[imageName] = this.s3.getProductPhotoUrl(
+        `misc/${imageName}.jpg`
+      );
+    });
+  }
 }
--- src/app/welcome-page/welcome.component.ts
+++ src/app/welcome-page/welcome.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { S3Services } from '../services/s3.service';
 
 @Component({
   selector: 'welcomes-root',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css'],
 })
-export class WelcomeComponent {}
+export class WelcomeComponent implements OnInit {
+  constructor(private s3: S3Services) {}
+
+  imageLinks: { [imageName: string]: string } | undefined;
+  link: string | undefined;
+
+  ngOnInit(): void {
+    this.getImageLinks();
+  }
+
+  getImageLinks() {
+    let imageNames = ['farm', 'farm_1', 'farm_2', 'farm_5'];
+    let imageLinks: { [imageName: string]: string } = {};
+
+    imageNames.forEach((imageName) => {
+      imageLinks[imageName] = this.s3.getProductPhotoUrl(
+        `misc/${imageName}.jpg`
+      );
+    });
+
+    this.imageLinks = imageLinks;
+  }
+}
