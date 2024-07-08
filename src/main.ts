4b170803914ee81ef5616db8f36b17af4e0849ef
Dat Le <73367400+datle2001@users.noreply.github.com>
2024-07-07 17:31:01 -0700

    ucomment

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,66 @@
 /// <reference types="@angular/localize" />
 
+import { provideHttpClient } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter, Routes } from '@angular/router';
 import { provideToastr } from 'ngx-toastr';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
-import { ConfirmationPageComponent } from '@components/confirmation-page/confirmation-page.component';
 
 export const routes: Routes = [
-  { path: 'products', 
-    loadComponent: () => import('@components/products-page/products-page.component').then((m) => m.ProductsPageComponent) 
+  {
+    path: 'products',
+    loadComponent: () =>
+      import('@components/products-page/products-page.component').then(
+        (m) => m.ProductsPageComponent
+      ),
   },
-  { path: 'products/:id', 
-    loadComponent: () => import('@components/product-detail-page/product-detail-page.component').then((m) => m.ProductDetailPageComponent)  
+  {
+    path: 'products/:id',
+    loadComponent: () =>
+      import(
+        '@components/product-detail-page/product-detail-page.component'
+      ).then((m) => m.ProductDetailPageComponent),
   },
-  { path: 'cart', 
-    loadComponent: () => import('@components/cart/cart.component').then((m) => m.CartComponent )
+  {
+    path: 'cart',
+    loadComponent: () =>
+      import('@components/cart/cart.component').then((m) => m.CartComponent),
   },
-  { path: 'support',
-    loadComponent: () => import('@components/support-page/support-page.component').then((m) => m.SupportComponent)
+  {
+    path: 'support',
+    loadComponent: () =>
+      import('@components/support-page/support-page.component').then(
+        (m) => m.SupportComponent
+      ),
   },
-  { path: 'confirmation/:id', component: ConfirmationPageComponent,
-    // loadComponent: () => import('@components/confirmation-page/confirmation-page.component').then((m) => m.ConfirmationPageComponent) 
+  {
+    path: 'confirmation/:id',
+    loadComponent: () =>
+      import('@components/confirmation-page/confirmation-page.component').then(
+        (m) => m.ConfirmationPageComponent
+      ),
   },
-  { path: 'login',
-    loadComponent: () => import('@components/login-page/login-page.component').then((m) => m.LoginPageComponent)
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('@components/login-page/login-page.component').then(
+        (m) => m.LoginPageComponent
+      ),
   },
   {
     path: 'user',
-    loadComponent: () => import('@components/user-page/user-page.component').then((m) => m.UserPageComponent)
+    loadComponent: () =>
+      import('@components/user-page/user-page.component').then(
+        (m) => m.UserPageComponent
+      ),
   },
-  { path: '', 
-    loadComponent: () => import('@components/home-page/home-page.component').then((m) => m.HomePageComponent) 
+  {
+    path: '',
+    loadComponent: () =>
+      import('@components/home-page/home-page.component').then(
+        (m) => m.HomePageComponent
+      ),
   },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
