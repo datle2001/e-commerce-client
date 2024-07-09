5596b964a408163538f5589d65dabdc25ff8fb95
Dat Le <73367400+datle2001@users.noreply.github.com>
2024-07-09 15:39:42 -0700

    delete s3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,6 @@ export const environment = {
   api: {
     url: 'http://localhost:8000/api/v1',
   },
-
-  // s3: {
-  //   credentials: {
-  //     accessKeyId: 'AKIAZH3DM7T7FEHHV64G',
-  //     region: 'us-west-1',
-  //     secretAccessKey: '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',
-  //   },
-
-  //   photoBucket: 'ecommerce-product-photos',
-  // },
-
+  
   googleStorageURL: 'https://storage.googleapis.com/dat-e-commerce',
 };
