2bb3bf0af997dc8176ddac5f8e08d54017dcbec9
Dat Le <73367400+datle2001@users.noreply.github.com>
2024-07-10 18:44:40 -0700

    home page redesigned

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { RouterLink } from '@angular/router';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -8,15 +10,12 @@ import { environment } from 'src/environments/environment';
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css'],
   standalone: true,
-  imports: []
+  imports: [MatButtonModule, RouterLink]
 })
 export class HomePageComponent implements OnInit {
   constructor() {}
 
   protected welcomePageImageLink = `${environment.googleStorageURL}/image-welcome-page/`;
 
-  ngOnInit(): void {
-    console.log(true);
-    
-  }
+  ngOnInit(): void {}
 }
