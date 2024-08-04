85f0494bc5c62377db14c06cb00e2b904bdf598f
Dat Le <73367400+datle2001@users.noreply.github.com>
2024-08-03 22:04:02 -0700

    Delete src/environments directory

--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  api: {
-    url: 'https://dat-ecommerce-api-ceca23ed40df.herokuapp.com/api/v1',
-  },
-  googleStorageURL: 'https://storage.googleapis.com/dat-e-commerce',
-};
